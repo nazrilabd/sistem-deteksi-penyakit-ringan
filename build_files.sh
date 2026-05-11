10ffc2b302eab735456b1187cc2de10ee201e091
nazril abd <nazrilabdillah106@gmail.com>
2026-05-11 10:31:20 +0700

    a

--- build_files.sh
+++ build_files.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 # # Pastikan pip terinstal
-# python3 -m pip install --upgrade pip
+python3 -m pip install --upgrade pip
 
 # # Instal semua dependensi
-# python3 -m pip install -r requirements.txt
+python3 -m pip install -r requirements.txt
 
 # PENTING: Kumpulkan Static Files
 # --noinput agar tidak ada prompt
 # --clear untuk menghapus file lama
 # echo "Collecting static files..."
-# python3 manage.py collectstatic --noinput --clear
+python3 manage.py collectstatic --noinput --clear
 
 # # Jalankan migrasi database (jika ada perubahan model)
-# echo "Running migrations..."
-# python3 manage.py migrate --noinput
+echo "Running migrations..."
+python3 manage.py migrate --noinput
